ca64611271e509908896d4ae0128f1d838799101
Kimani <elvis.kimani@student.moringaschool.com>
2022-07-14 22:01:59 +0300

    commit tree 0b7e4c2e6ed154ba1a1f73255346c0c495525a05

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+
+import React from "react";
+
+function EyesOnMe (){
+
+    function handleFocus(e){
+        e.preventDefault();
+        console.log("Good!")
+    }
+
+    function handleBlur(e){
+        e.preventDefault();
+        console.log("Hey! Eyes on me!")
+    }
+    return (
+        <div>
+            <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+        </div>
+        
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,19 @@
 // Code Keypad Component Here
+
+import React from "react";
+
+function Keypad () {
+    
+    return(
+        <>
+        <input 
+        type="password" 
+        name="pwd"
+        onChange={() => console.log("Entering password...")}
+        placeholder = "Enter password"
+        />
+        </>
+    )
+}
+
+export default Keypad;
